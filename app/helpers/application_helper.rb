2ea0462a6bdea3fdfcdc73faca72b7f76afdffe2
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-09 00:17:22 -0300

    deleted game helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  require 'elo'
-
-  def random_game
-
-  end
-
-  def run_game(player_win, player_loser)
-    winner = Elo::Player.new(rating: player_win.rating.rating, games_played: player_win.rating.games_played)
-    loser = Elo::Player.new(rating: player_loser.rating.rating, games_played: player_loser.rating.games_played)   
-    
-    winner.wins_from(loser)
-    player_win.update_rating(winner)
-    player_loser.update_rating(loser)
-  end
-
-  
-  
 end
