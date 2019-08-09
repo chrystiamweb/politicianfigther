5bb53098161f2f99f6191aab8e80bef3caaad49a
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-09 00:16:39 -0300

    created updated_ranking and set_player methods

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,15 +1,26 @@
 class PoliticiansController < ApplicationController
   before_action :set_politician, only: [:show, :edit, :update, :destroy]
 
-  # GET /politicians
-  # GET /politicians.json
-  def index
+  def index    
     @player_one = Politician.find(rand(1..Politician.count))
-    @player_two = Politician.where.not(id: @player_one.id).sample
+    @player_two = Politician.where.not(id: @player_one.id).sample    
   end
 
   def ranking
     @rankings = Rating.all.order(games_played: :desc)
+  end
+
+  def update_ranking
+    winner = set_player(params[:winner])
+    loser = set_player(params[:loser])
+    GameService.new.run_game(winner, loser)
+    redirect_to root_url
+  end
+
+  private
+
+  def set_player(id)
+    Politician.find(id)
   end
 
 end
