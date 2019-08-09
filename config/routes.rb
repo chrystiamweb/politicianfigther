b41041cf679271b92e8a31dde5bbb9d6303ac19e
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-09 00:18:58 -0300

    add update_ranking route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'politicians#index'
   get '/ranking', to: 'politicians#ranking'
+  get '/update_ranking', to: 'politicians#update_ranking'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
